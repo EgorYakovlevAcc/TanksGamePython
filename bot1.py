2c5c01cfb5d10eaaa0f1eaaaf240ea2cb3dea02b
EgorYakovlevAcc <EgorYakovlevAcc@yandex.ru>
2018-03-17 11:50:05 +0300

    square and direction
    add new rules

--- bot1.py
+++ bot1.py
@@ -1,23 +1,33 @@
-from sympy.strategies.core import switch
-
 n = 0
-k = 0
+
+def checkBorder (x, y):
+    if (x > 100) and (x < 500):
+        if (y > 100) and (y < 500):
+            n = n + 100
+    else:
+        n = n + 100
+
 
 def move(me, enemies, bullets, bonuses, m):
 
     global n
     n = n + 1
 
-    if (n // 100) % 4 == 0:
-        m.left()
+    if bonuses:
+        m.dir(bonuses[0]['pos'][0] - me['pos'][0], bonuses[0]['pos'][1] - me['pos'][1])
 
-    if (n // 100) % 4 == 1:
-        m.up()
+    else:
 
-    if (n // 100) % 4 == 2:
-        m.right()
+        if (n // 100) % 4 == 0:
+            m.left()
 
-    if (n // 100) % 4 == 3:
-        m.down()
+        if (n // 100) % 4 == 1:
+            m.up()
+
+        if (n // 100) % 4 == 2:
+            m.right()
+
+        if (n // 100) % 4 == 3:
+            m.down()
 
     m.shot(enemies[0]['pos'][0], enemies[0]['pos'][1])
